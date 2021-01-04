c17b30e7527f25da3ad2fedebedebcf364d57e47
timrijckaert <tim.rijckaert@gmail.com>
2021-01-04 19:54:53 +0100

    Add :vlaamsetv app

---
+++ vlaamsetv/src/main/java/be/tapped/vlaamsetv/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.tapped.vlaamsetv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
