a5704ebee4d83da0cf0753f9c94a879ae74ab7aa
timrijckaert <tim.rijckaert@gmail.com>
2021-01-11 18:38:12 +0100

    Remove unused onCreate method

--- vlaamsetv/src/main/java/be/tapped/vlaamsetv/TvMainActivity.kt
+++ vlaamsetv/src/main/java/be/tapped/vlaamsetv/TvMainActivity.kt
@@ -1,11 +1,5 @@
 package be.tapped.vlaamsetv
 
-import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 
-class TvMainActivity : FragmentActivity(R.layout.activity_tv_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class TvMainActivity : FragmentActivity(R.layout.activity_tv_main)
