4e27112ed08653a408cb43d23b37a33fe25d8460
timrijckaert <tim.rijckaert@gmail.com>
2021-01-14 18:43:36 +0100

    VTM GO: Correct TokenWrapper Serializer

--- vlaamsetv/src/main/java/be/tapped/vlaamsetv/prefs/vtm/Serializers.kt
+++ vlaamsetv/src/main/java/be/tapped/vlaamsetv/prefs/vtm/Serializers.kt
@@ -1,12 +1,12 @@
 package be.tapped.vlaamsetv.prefs.vtm
 
-import be.tapped.vlaamsetv.auth.vtm.TokenWrapper
 import be.tapped.vlaamsetv.auth.vtm.VTMGOCredentials
+import be.tapped.vlaamsetv.auth.vtm.VTMTokenWrapper
 import be.tapped.vlaamsetv.prefs.Crypto
 import be.tapped.vlaamsetv.prefs.EncryptedProtoSerializer
 
 class TokenWrapperSerializer(crypto: Crypto) :
-    EncryptedProtoSerializer<TokenWrapper>(crypto, TokenWrapper(), TokenWrapper.ADAPTER)
+    EncryptedProtoSerializer<VTMTokenWrapper>(crypto, VTMTokenWrapper(), VTMTokenWrapper.ADAPTER)
 
 class VTMCredentialsSerializer(crypto: Crypto) :
     EncryptedProtoSerializer<VTMGOCredentials>(
