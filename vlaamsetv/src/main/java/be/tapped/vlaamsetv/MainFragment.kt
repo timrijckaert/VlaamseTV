a45436733d13d428b7b852673540f81491f0badf
timrijckaert <tim.rijckaert@gmail.com>
2021-01-15 21:16:37 +0100

    Check the root navigation in correct lifecycle

--- vlaamsetv/src/main/java/be/tapped/vlaamsetv/MainFragment.kt
+++ vlaamsetv/src/main/java/be/tapped/vlaamsetv/MainFragment.kt
@@ -1,17 +1,15 @@
 package be.tapped.vlaamsetv
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
 class MainFragment(private val rootNavigator: RootNavigator) : Fragment(R.layout.fragment_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            lifecycleScope.launch {
-                rootNavigator.moveToStartDestination()
-            }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            rootNavigator.moveToStartDestination()
         }
     }
 }
