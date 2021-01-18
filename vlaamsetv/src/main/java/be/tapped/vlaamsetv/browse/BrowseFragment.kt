50f5c9b60df2b1e543b4a876a7812f07a96e21d1
Tim Rijckaert <tim.rijckaert@gmail.com>
2021-01-18 16:31:47 +0100

    Toying around with real stuff

--- vlaamsetv/src/main/java/be/tapped/vlaamsetv/browse/BrowseFragment.kt
+++ vlaamsetv/src/main/java/be/tapped/vlaamsetv/browse/BrowseFragment.kt
@@ -1,19 +1,24 @@
 package be.tapped.vlaamsetv.browse
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.DividerRow
 import androidx.leanback.widget.HeaderItem
 import androidx.leanback.widget.ImageCardView
 import androidx.leanback.widget.ListRow
 import androidx.leanback.widget.ListRowPresenter
-import androidx.leanback.widget.PageRow
 import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.SectionRow
+import androidx.lifecycle.lifecycleScope
 import be.tapped.vlaamsetv.R
+import be.tapped.vrtnu.content.Program
+import be.tapped.vrtnu.content.VRTApi
+import coil.load
+import kotlinx.coroutines.launch
 
 class SomeFragment : Fragment(R.layout.fragment_some), BrowseSupportFragment.MainFragmentAdapterProvider {
 
@@ -32,52 +37,39 @@ class BrowseFragment : BrowseSupportFragment() {
         mCategoryRowAdapter = ArrayObjectAdapter(ListRowPresenter())
         adapter = mCategoryRowAdapter
 
-        mainFragmentRegistry.registerFragment(PageRow::class.java,
-            object : BrowseSupportFragment.FragmentFactory<Fragment>() {
-                override fun createFragment(row: Any?): Fragment {
-                    return SomeFragment()
+        val vrtSection = SectionRow(HeaderItem("VRT NU"))
+        val vrtAZProgramsObjectAdapter = ArrayObjectAdapter(object : Presenter() {
+            override fun onCreateViewHolder(parent: ViewGroup): ViewHolder = ViewHolder(ImageCardView(requireContext()))
+
+            override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+                val program = item as Program
+                with(viewHolder.view as ImageCardView) {
+                    titleText = program.title
+                    contentText = program.description
+                    val width: Int = resources.getDimensionPixelSize(R.dimen.card_width)
+                    val height: Int = resources.getDimensionPixelSize(R.dimen.card_height)
+                    setMainImageDimensions(width, height)
+                    mainImageView.load("https:${program.thumbnail}")
                 }
-            })
-
-        val headerItem = HeaderItem("Google+")
-        val arrayObjectAdapter = ArrayObjectAdapter(object : Presenter() {
-            override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
-                val cardView: ImageCardView = object : ImageCardView(parent.context) {
-                    override fun setSelected(selected: Boolean) {
-                        updateCardBackgroundColor(this, selected)
-                        super.setSelected(selected)
-                    }
-                }
-
-                cardView.isFocusable = true
-                cardView.isFocusableInTouchMode = true
-                updateCardBackgroundColor(cardView, false)
-                return ViewHolder(cardView)
-            }
-
-            private fun updateCardBackgroundColor(view: ImageCardView, selected: Boolean) {
-                val color: Int = if (selected) Color.RED else Color.GREEN
-
-                view.setBackgroundColor(color)
-                view.findViewById<View>(R.id.info_field).setBackgroundColor(color)
-            }
-
-            override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
-                val cardView = viewHolder.view as ImageCardView
-                cardView.titleText = "$item"
-                cardView.contentText = "Content text"
             }
 
             override fun onUnbindViewHolder(viewHolder: ViewHolder?) {
 
             }
         })
-        arrayObjectAdapter.add("This")
-        arrayObjectAdapter.add("is")
-        arrayObjectAdapter.add("a")
-        arrayObjectAdapter.add("test")
-        val listRow = ListRow(headerItem, arrayObjectAdapter)
-        mCategoryRowAdapter.add(PageRow(headerItem))
-    }
+        val vrtAZPrograms = ListRow(HeaderItem("AZ"), vrtAZProgramsObjectAdapter)
 
+        val divider = DividerRow()
+
+        val vrtApi = VRTApi()
+        lifecycleScope.launch {
+            val vrtPrograms = vrtApi.fetchAZPrograms().orNull()?.programs ?: emptyList()
+
+            vrtPrograms.forEach {
+                vrtAZProgramsObjectAdapter.add(it)
+            }
+        }
+        mCategoryRowAdapter.addAll(0, listOf(vrtSection, vrtAZPrograms, divider))
+    }
 }
+
